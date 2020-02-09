307e6a55cc14b0f2f06d1a1948b6824139b0d089
Dallas Bobryk <dallasbobryk@gmail.com>
2020-02-08 20:17:19 -0600

    Fix repo

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,15 +1,22 @@
 package repo
 
+import (
+	"context"
+	"time"
+
+	"firebase.google.com/go/db"
+)
+
 type repo struct {
-	firebaseClient *db.Config
+	firebaseClient *db.Client
 }
 
 type RepoInterface interface {
-	SavePost(contex.Context, string, string, string, string, time.Time, bool) error
+	SavePost(context.Context, string, string, string, string, time.Time, bool) error
 }
 
 func NewRepo(firebaseClient *db.Client) *repo {
-	return repo{
-		firebaseClient: firebaseClient
+	return &repo{
+		firebaseClient: firebaseClient,
 	}
 }
